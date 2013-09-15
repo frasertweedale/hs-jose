d0f4d84e25f35ab1aceeba6a4925013a35d23d8f
Fraser Tweedale <frase@frase.id.au>
2013-09-15 18:22:41 +1000

    Types: add Base64UrlString
    JWA §4.7.1. defines the "apu" (Agreement PartyUInfo) and "apv"
    (Agreement PartyVInfo) ECDH Key Agreement header parameters, which
    are defined as base64url encoded strings.
    
    Add HSpec based on examples given in the JWA specification and
    implement the Base64UrlString type and its FromJSON instance.  Note
    that "=" padding may be absent from the input so it is necessary to
    equals-pad the input to a multiple of four characters.

---
+++ Crypto/JOSE/Types.hs
@@ -0,0 +1,40 @@
+-- This file is part of jose - web crypto library
+-- Copyright (C) 2013  Fraser Tweedale
+--
+-- jose is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Crypto.JOSE.Types where
+
+import Control.Applicative
+import Data.Char
+import Data.Word
+
+import qualified Codec.Binary.Base64Url as B64
+import Data.Aeson
+import qualified Data.Text as T
+
+
+data Base64UrlString = Base64UrlString String
+  deriving (Eq, Show)
+
+instance FromJSON Base64UrlString where
+  parseJSON (String s) = case B64.decode $ equalsPad $ T.unpack s of
+      Nothing -> fail "invalid base64url encoded string"
+      -- probably wrong; really want to do a proper UTF-8 decode of bytes
+      Just bytes -> pure $ Base64UrlString $ map (chr . fromIntegral) bytes
+    where
+      equalsPad s
+        | length s `mod` 4 == 0 = s
+        | otherwise             = equalsPad (s ++ "=")
+  parseJSON _ = empty
