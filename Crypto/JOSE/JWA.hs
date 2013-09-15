d700162cbba1ba940a28bbd79bbdd6f050f4df92
Fraser Tweedale <frase@frase.id.au>
2013-09-15 15:50:53 +1000

    JWA: extract JWE parameters to own module JWA.JWE

--- Crypto/JOSE/JWA.hs
+++ Crypto/JOSE/JWA.hs
@@ -20,59 +20,19 @@
 module Crypto.JOSE.JWA where
 
 import Control.Applicative
-import Data.Tuple
-import GHC.Generics (Generic)
 
 import Data.Aeson
-import Data.Hashable
 import qualified Data.HashMap.Strict as M
 
+import qualified Crypto.JOSE.JWA.JWE as JWA.JWE
 import qualified Crypto.JOSE.JWA.JWS as JWA.JWS
 
 
-data JWEAlg =
-  RSA1_5                -- RSAES-PKCS1-V1_5                       Required
-  | RSA_OAEP            -- RSAES using OAEP                       Optional
-  | A128KW              -- AES Key Wrap                           Recommended
-  | A192KW              -- AES Key Wrap                           Optional
-  | A256KW              -- AES Key Wrap                           Recommended
-  | Dir                 -- direct use of symmetric key            Recommended
-  | ECDH_ES             -- ECDH Ephemeral Static                  Recommended+
-  | ECDH_ES_A128KW      --                                        Recommended
-  | ECDH_ES_A192KW      --                                        Optional
-  | ECDH_ES_A256KW      --                                        Recommended
-  | A128GCMKW           -- AES in Galois/Counter Mode             Optional
-  | A192GCMKW           -- AES in Galois/Counter Mode             Optional
-  | A256GCMKW           -- AES in Galois/Counter Mode             Optional
-  | PBES2_HS256_A128KW  -- PBES2 with HMAC SHA and AES Key Wrap   Optional
-  | PBES2_HS384_A128KW  -- PBES2 with HMAC SHA and AES Key Wrap   Optional
-  | PBES2_HS512_A128KW  -- PBES2 with HMAC SHA and AES Key Wrap   Optional
-  deriving (Show)
-
-jweAlgMap = M.fromList [
-  ("RSA1_5",              RSA1_5),
-  ("RSA-OAEP",            RSA_OAEP),
-  ("A128KW",              A128KW),
-  ("A192KW",              A192KW),
-  ("A256KW",              A256KW),
-  ("dir",                 Dir),
-  ("ECDH-ES",             ECDH_ES),
-  ("ECDH-ES+A128KW",      ECDH_ES_A128KW),
-  ("ECDH-ES+A192KW",      ECDH_ES_A192KW),
-  ("ECDH-ES+A256KW",      ECDH_ES_A256KW),
-  ("A128GCMKW",           A128GCMKW),
-  ("A192GCMKW",           A192GCMKW),
-  ("A256GCMKW",           A256GCMKW),
-  ("PBES2-HS256+A128KW",  PBES2_HS256_A128KW),
-  ("PBES2-HS384+A128KW",  PBES2_HS384_A128KW),
-  ("PBES2-HS512+A128KW",  PBES2_HS512_A128KW)
-  ]
-
-data Alg = JWSAlg JWA.JWS.Alg | JWEAlg JWEAlg
+data Alg = JWSAlg JWA.JWS.Alg | JWEAlg JWA.JWE.Alg
   deriving (Show)
 
 instance FromJSON Alg where
-  parseJSON (String s) = case M.lookup s jweAlgMap of
+  parseJSON (String s) = case M.lookup s JWA.JWE.algMap of
     Just v -> pure $ JWEAlg v
     Nothing -> case M.lookup s JWA.JWS.algMap of
       Just v -> pure $ JWSAlg v
