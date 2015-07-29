1daaa34bd8354f7bfd121cb6c4a805f77b67c162
Fraser Tweedale <frase@frase.id.au>
2015-07-29 18:27:46 +1000

    tests: add props for HMAC and RSA gen, sign and verify

--- test/Properties.hs
+++ test/Properties.hs
@@ -34,7 +34,9 @@ import Crypto.JOSE.JWS
 properties = testGroup "Properties"
   [ testProperty "SizedBase64Integer round-trip" $
     \(n :: SizedBase64Integer) -> decode (encode [n]) == Just [n]
-  , testProperty "EC gen, sign and verify" prop_ecSignAndVerify
+  , testProperty "ECDSA gen, sign and verify" prop_ecSignAndVerify
+  , testProperty "HMAC gen, sign and verify" prop_hmacSignAndVerify
+  , testProperty "RSA gen, sign and verify" prop_rsaSignAndVerify
   ]
 
 prop_ecSignAndVerify :: Crv -> B.ByteString -> Property
@@ -42,10 +44,30 @@ prop_ecSignAndVerify crv msg = monadicIO $ do
   k :: JWK <- run $ gen (ECGenParam crv)
   let alg = case crv of P_256 -> ES256 ; P_384 -> ES384 ; P_521 -> ES512
   signResult <- run $ signJWS (newJWS msg) (newJWSHeader alg) k
-  case signResult of
-    Left e -> do
-      monitor (counterexample $ "Failed to sign: " ++ show e)
-      assert False
-    Right jws -> do
-      monitor (counterexample "Failed to verify")
-      assert (verifyJWS def def k jws)
+  checkSignJWS k signResult
+
+prop_hmacSignAndVerify :: B.ByteString -> Property
+prop_hmacSignAndVerify msg = monadicIO $ do
+  keylen <- run $ generate arbitrarySizedNatural
+  k :: JWK <- run $ gen (OctGenParam keylen)
+  alg <- run $ generate $ oneof $ pure <$> [HS256, HS384, HS512]
+  signResult <- run $ signJWS (newJWS msg) (newJWSHeader alg) k
+  checkSignJWS k signResult
+
+prop_rsaSignAndVerify :: B.ByteString -> Property
+prop_rsaSignAndVerify msg = monadicIO $ do
+  keylen <- run $ generate $ oneof $ pure . (`div` 8) <$> [2048, 3072, 4096]
+  k :: JWK <- run $ gen (RSAGenParam keylen)
+  alg <- run $ generate $
+    oneof $ pure <$> [RS256, RS384, RS512, PS256, PS384, PS512]
+  signResult <- run $ signJWS (newJWS msg) (newJWSHeader alg) k
+  checkSignJWS k signResult
+
+checkSignJWS :: (Monad m, Show e) => JWK -> Either e JWS -> PropertyM m ()
+checkSignJWS k signResult = case signResult of
+  Left e -> do
+    monitor (counterexample $ "Failed to sign: " ++ show e)
+    assert False
+  Right jws -> do
+    monitor (counterexample "Failed to verify")
+    assert (verifyJWS def def k jws)
